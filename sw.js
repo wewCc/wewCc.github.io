11314793a7a1d084c057721828636c37851ef04c
cc123CcSty <1935596281@qq.com>
2024-12-23 23:33:55 +0800

    Site updated: 2024-12-23 23:33:54

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1734967874667";const preCache = ["/images/taichi.png","/images/banner2.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1734968031385";const preCache = ["/images/taichi.png","/images/banner2.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
