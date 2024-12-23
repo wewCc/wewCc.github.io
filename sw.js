31b6b4d52c22269d48b883c4b43e06bdd207631c
cc123CcSty <1935596281@qq.com>
2024-12-23 23:18:45 +0800

    Site updated: 2024-12-23 23:18:44

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1734831992204";const preCache = ["/images/taichi.png","/images/banner2.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1734967120929";const preCache = ["/images/taichi.png","/images/banner2.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
